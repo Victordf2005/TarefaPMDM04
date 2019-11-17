3d2f5c75432cbaa87c128ece9e43ed94f31491ef
Víctor <victordf2005@gmaillcom>
2019-11-17 18:57:54 +0100

    Imaxes dependendo da pantalla

--- app/src/main/java/tenda/tarefa_02/Cliente.java
+++ app/src/main/java/tenda/tarefa_02/Cliente.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -77,5 +78,8 @@ public class Cliente extends AppCompatActivity {
         setContentView(R.layout.activity_cliente);
 
         xestionarEventos();
+
+
     }
+
 }
